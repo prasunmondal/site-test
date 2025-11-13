42b52c918033582883f4642739e146c6ce6c86ae
Prasun Mondal <prasun.mondal02@gmail.com>
2025-11-13 20:53:51 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,35 @@
 const CACHE_NAME = "mb-poultry-cache-v1";
+
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./images/payment-qr.png",
-  "./images/icon-192.png",
-  "./images/icon-512.png"
+  "/site-test/",
+  "/site-test/index.html",
+  "/site-test/style.css",
+  "/site-test/script.js",
+  "/site-test/images/payment-qr.png",
+  "/site-test/images/icon-192.png",
+  "/site-test/images/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
     )
   );
 });
